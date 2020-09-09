5a15acf53b9d563c587989487738762f603f53f7
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-08 21:04:46 -0500

    Recalibrate test_optim and keep realistic precision

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -67,40 +67,40 @@ def test_minimize(tensorlib, precision, optimizer, do_grad, do_stitch):
             'no_grad-scipy-tensorflow-32b': [1.0, 1.0],
             'no_grad-scipy-jax-32b': [1.0, 1.0],
             # no grad, scipy, 64b
-            'no_grad-scipy-numpy-64b': [0.49998815367220306, 0.9999696999038924],
-            'no_grad-scipy-pytorch-64b': [0.49998815367220306, 0.9999696999038924],
-            'no_grad-scipy-tensorflow-64b': [0.49998865164653106, 0.9999696533705097],
-            'no_grad-scipy-jax-64b': [0.4999880886490433, 0.9999696971774877],
+            'no_grad-scipy-numpy-64b': [0.4999985, 1.00000122],
+            'no_grad-scipy-pytorch-64b': [0.4999985, 1.00000122],
+            'no_grad-scipy-tensorflow-64b': [0.49999869, 1.00000121],
+            'no_grad-scipy-jax-64b': [0.49999894, 1.00000114],
             # do grad, scipy, 32b
-            'do_grad-scipy-pytorch-32b': [0.49993881583213806, 1.0001085996627808],
-            'do_grad-scipy-tensorflow-32b': [0.4999384582042694, 1.0001084804534912],
-            'do_grad-scipy-jax-32b': [0.4999389052391052, 1.0001085996627808],
+            'do_grad-scipy-pytorch-32b': [0.49999850, 1.00000131],
+            'do_grad-scipy-tensorflow-32b': [0.49999896, 1.0000011],
+            'do_grad-scipy-jax-32b': [0.49999964, 1.0],
             # do grad, scipy, 64b
-            'do_grad-scipy-pytorch-64b': [0.49998837853531425, 0.9999696648069287],
-            'do_grad-scipy-tensorflow-64b': [0.4999883785353142, 0.9999696648069278],
-            'do_grad-scipy-jax-64b': [0.49998837853531414, 0.9999696648069285],
+            'do_grad-scipy-pytorch-64b': [0.499998588, 1.000001218],
+            'do_grad-scipy-tensorflow-64b': [0.49999859, 1.00000122],
+            'do_grad-scipy-jax-64b': [0.49999859, 1.00000122],
             # no grad, minuit, 32b - not very consistent for pytorch
-            'no_grad-minuit-numpy-32b': [0.49622172117233276, 1.0007264614105225],
+            'no_grad-minuit-numpy-32b': [0.49928552, 0.9998213],
             #    nb: macos gives different numerics than CI
             # 'no_grad-minuit-pytorch-32b': [0.7465415000915527, 0.8796938061714172],
             'no_grad-minuit-pytorch-32b': [0.9684963226318359, 0.9171305894851685],
-            'no_grad-minuit-tensorflow-32b': [0.5284154415130615, 0.9911751747131348],
+            'no_grad-minuit-tensorflow-32b': [0.5073193, 0.99639267],
             # 'no_grad-minuit-jax-32b': [0.5144518613815308, 0.9927923679351807],
             'no_grad-minuit-jax-32b': [0.49620240926742554, 1.0018986463546753],
             # no grad, minuit, 64b - quite consistent
-            'no_grad-minuit-numpy-64b': [0.5000493563629738, 1.0000043833598724],
-            'no_grad-minuit-pytorch-64b': [0.5000493563758468, 1.0000043833508256],
-            'no_grad-minuit-tensorflow-64b': [0.5000493563645547, 1.0000043833598657],
-            'no_grad-minuit-jax-64b': [0.5000493563528641, 1.0000043833614634],
+            'no_grad-minuit-numpy-64b': [0.50280348, 0.99887193],
+            'no_grad-minuit-pytorch-64b': [0.50280348, 0.99887193],
+            'no_grad-minuit-tensorflow-64b': [0.50280348, 0.99887193],
+            'no_grad-minuit-jax-64b': [0.50280348, 0.99887193],
             # do grad, minuit, 32b
-            'do_grad-minuit-pytorch-32b': [0.5017611384391785, 0.9997190237045288],
-            'do_grad-minuit-tensorflow-32b': [0.5012885928153992, 1.0000673532485962],
+            'do_grad-minuit-pytorch-32b': [0.50013715, 0.99995589],
+            'do_grad-minuit-tensorflow-32b': [0.50124216, 0.99938375],
             # 'do_grad-minuit-jax-32b': [0.5029529333114624, 0.9991086721420288],
             'do_grad-minuit-jax-32b': [0.5007095336914062, 0.9999282360076904],
             # do grad, minuit, 64b
-            'do_grad-minuit-pytorch-64b': [0.500273961181471, 0.9996310135736226],
-            'do_grad-minuit-tensorflow-64b': [0.500273961167223, 0.9996310135864218],
-            'do_grad-minuit-jax-64b': [0.5002739611532436, 0.9996310135970794],
+            'do_grad-minuit-pytorch-64b': [0.50111033, 0.99953986],
+            'do_grad-minuit-tensorflow-64b': [0.50111034, 0.99953987],
+            'do_grad-minuit-jax-64b': [0.50111034, 0.99953987],
         }[identifier]
 
         result = pyhf.infer.mle.fit(data, m, do_grad=do_grad, do_stitch=do_stitch)
